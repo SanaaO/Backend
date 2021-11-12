f26deb14908187e7869a8c8d15ffc87284b7003c
SanaaO <saana.oueslati@gmail.com>
2021-11-12 15:37:04 +0100

    Add getAll and delete 3D object

--- app/controllers/shapeController.js
+++ app/controllers/shapeController.js
@@ -31,3 +31,27 @@ exports.findByID = (req, res) => {
     res.status(400).send({ message: " missing id param !" });
   }
 };
+
+//get all 3D objects stored in database
+exports.findAll = (req, res) => {
+  Shape.find().then((result) => {
+    res.status(200).json({ status: "Success", data: result });
+  });
+};
+
+exports.delete = (req, res) => {
+  const id = req.query.id;
+  if (id) {
+    Shape.findByIdAndDelete(id).then((result) => {
+      res.status(200).json({ statuts: "Success", data: result });
+    });
+  } else {
+    res.status(400).send({ message: " missing id param !" });
+  }
+};
+
+exports.deleteAll = (req, res) => {
+  Shape.deleteMany().then((result) => {
+    res.status(200).json({ status: "Success", data: result });
+  });
+};
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -5,5 +5,8 @@ const Router = express.Router();
 Router.post("/add", shapeController.addShape);
 Router.put("/update", shapeController.update);
 Router.get("/get", shapeController.findByID);
+Router.get("/getAll", shapeController.findAll);
+Router.delete("/delete", shapeController.delete);
+Router.delete("/deleteAll", shapeController.deleteAll);
 
 module.exports = Router;

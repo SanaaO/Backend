099917fa0c19053133f2527994d9fb3846e74558
SanaaO <saana.oueslati@gmail.com>
2021-11-12 15:25:08 +0100

    Add get 3D object

--- app/controllers/shapeController.js
+++ app/controllers/shapeController.js
@@ -1,5 +1,6 @@
 const Shape = require("../models/shape");
 
+//create new 3D object and store it into database
 exports.addShape = (req, res) => {
   const shape = new Shape(req.body);
   shape.save().then((result) => {
@@ -7,10 +8,23 @@ exports.addShape = (req, res) => {
   });
 };
 
+//update 3D object with selected id
 exports.update = (req, res) => {
   const id = req.query.id;
   if (id) {
     Shape.findByIdAndUpdate(id, req.body, { new: true }).then((result) => {
+      res.status(200).json({ statuts: "Success", data: result });
+    });
+  } else {
+    res.status(400).send({ message: " missing id param !" });
+  }
+};
+
+//retreive 3D Object stored in database by id
+exports.findByID = (req, res) => {
+  const id = req.query.id;
+  if (id) {
+    Shape.findById(id).then((result) => {
       res.status(200).json({ statuts: "Success", data: result });
     });
   } else {
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,5 +4,6 @@ const Router = express.Router();
 
 Router.post("/add", shapeController.addShape);
 Router.put("/update", shapeController.update);
+Router.get("/get", shapeController.findByID);
 
 module.exports = Router;
